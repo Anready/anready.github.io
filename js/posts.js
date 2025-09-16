d0d6a1af0f85ff13cc2d3b991de9a692ddf1fa7e
Anready <104269567+Anready@users.noreply.github.com>
2025-09-16 13:00:55 +0300

    Update posts.js

--- js/posts.js
+++ js/posts.js
@@ -1,13 +1,15 @@
 const postsRu = [
     { title: "Привет всем", content: "Здесь будут интересные и полезные посты. Добро пожаловать!" },
     { title: "Обновление", content: "Первая стабильная версия блога"},
-    { title: "Тестовый пост", content: "Пост про ассемблер от ChatGPT", link: "posts/test.html" }
+    { title: "Тестовый пост", content: "Пост про ассемблер от ChatGPT", link: "posts/test.html" },
+    { title: "Xiaomi скрывает возможности камеры: расследование", content: "Как Xiaomi ограничивает возможности камеры", link: "posts/xiaomi-camera-investigation-ru.html" }
 ].reverse();
 
 const postsEn = [
     { title: "Hello Everyone", content: "Here will be interesting and useful posts. Welcome!" },
     { title: "Update", content: "First stable version of the blog" },
-    { title: "Test Post", content: "Post about assembler from ChatGPT", link: "posts/test.html" }
+    { title: "Test Post", content: "Post about assembler from ChatGPT", link: "posts/test.html" },
+    { title: "Xiaomi Hides the Camera Capabilities: An Investigation", content: "How Xiaomi restrict camera capabilities", link: "posts/xiaomi-camera-investigation-en.html"}
 ].reverse();
 
 
